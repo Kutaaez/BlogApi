46914e1e4db0912e76f7efd67bb241440ddf62a6
Zharaskhan <S23067305@narxoz.kz>
2025-12-27 16:06:24 +0600

    Created a basic project

---
+++ src/main/java/kz/seppaku/postmanBlog/PostmanBlogApplication.java
@@ -0,0 +1,13 @@
+package kz.seppaku.postmanBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmanBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostmanBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/seppaku/postmanBlog/PostmanBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seppaku.postmanBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
